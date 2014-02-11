a30fbb3998ace24440ecc72f7137936c67fb72b8
Josef Scheuer <webseppl@web.de>
2014-02-11 15:26:48 +0000

    string_ref.f90: added interfaces len, char                 added standard construction for interface str
    git-svn-id: https://subversion.assembla.com/svn/zappralot/trunk/libfortres@177 c37337d0-301c-4318-a67e-7f75229f9ee7

--- string_ref.f90
+++ string_ref.f90
@@ -4,13 +4,21 @@ module string_ref
   implicit none
 
   type, public, bind(C) :: StringRef
-    type (c_ptr) :: loc
-    integer*4    :: len
+    type (c_ptr) :: loc = c_null_ptr
+    integer*4    :: len = 0
   end type
 
   interface str
-    module procedure str_to_stringRef
+    module procedure str_to_stringRef, create_stringRef
     module procedure stringRef_to_str
+  end interface
+
+  interface char
+    module procedure stringRef_to_char
+  end interface
+
+  interface len
+    module procedure stringRef_len
   end interface
 
   abstract interface
@@ -23,17 +31,33 @@ module string_ref
   contains
 !-------------------
 
-  function str_to_stringRef( fstr ) result(cstr)
-    character(len=*), target, intent(in) :: fstr
-    type (StringRef)                     :: cstr
-    cstr%loc = c_loc(fstr)
-    cstr%len = len(fstr)
+  function create_stringRef() result(strRef)
+    type (StringRef) :: strRef
   end function
 
-  function stringRef_to_str( cstr ) result(fptr)
-    type (StringRef),     intent(in) :: cstr
-    character(len=cstr%len), pointer :: fptr
-    call c_f_pointer( cstr%loc, fptr )
+  function str_to_stringRef( fstr ) result(strRef)
+    character(len=*), target, intent(in) :: fstr
+    type (StringRef)                     :: strRef
+    strRef%loc = c_loc(fstr)
+    strRef%len = len(fstr)
+  end function
+
+  function stringRef_to_str( strRef ) result(fptr)
+    type (StringRef),       intent(in) :: strRef
+    character(len=strRef%len), pointer :: fptr
+    call c_f_pointer( strRef%loc, fptr )
+  end function
+
+  function stringRef_to_char( strRef ) result(cstr)
+    type (StringRef), intent(in) :: strRef
+    character(len=strRef%len)    :: cstr
+    cstr = str(strRef)
+  end function
+
+  pure function stringRef_len( strRef ) result(l)
+    type (StringRef), intent(in) :: strRef
+    integer*4                    :: l
+    l = strRef%len
   end function
 
 end module
