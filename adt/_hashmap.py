bab27d3e5f04ee9fcd35e0e79d271a266b2454cc
Josef Scheuer <webseppl@web.de>
2014-12-02 23:20:15 +0000

    added methods __len__, clear, and get to HashMap-binding class

--- adt/_hashmap.py
+++ adt/_hashmap.py
@@ -6,5 +6,17 @@ from _ftypes import mappedType
 
 @mappedType( 'hashmap', 'type(HashMap_t)' )
 class HashMap(Object):
-  pass
+
+	def __len__( self ):
+		return self.len_( byref(self) )
+	
+	def clear( self ):
+		self.clear_( byref(self) )
+
+
+	def get( self, key, default = None ):
+		ptr = POINTER(c_void_p)()
+		self.get_ptr_( byref(ptr), byref(self), c_char_p(key), c_int(len(key)) )
+		if ptr: return ptr.contents
+		else  : return default
 
