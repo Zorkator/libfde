6f89513f8b0562d1ed6cf919bbc3fbdd7e229c51
Josef Scheuer <webseppl@web.de>
2015-01-26 16:17:16 +0000

    Workarounds for string arrays and Windows AMD64 variable transfer

--- adt/_hashmap.py
+++ adt/_hashmap.py
@@ -10,13 +10,14 @@ class HashMap(Object):
 
 	def __len__( self ):
 		return self.len_( byref(self) )
-	
+
 	def clear( self ):
 		self.clear_( byref(self) )
 
 
 	def get( self, key, default = None ):
 		ptr = POINTER(Item)()
+		dummy = "{}".format(ptr) # de-optimize Python (????), so next line works
 		self.get_ptr_( byref(ptr), byref(self), c_char_p(key), c_int(len(key)) )
 		if ptr: return ptr.contents.value
 		else  : return default
