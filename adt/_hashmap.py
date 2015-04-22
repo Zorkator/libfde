0c0d72669bc5f003d795ce48db81a89ff19337af
Josef Scheuer <webseppl@web.de>
2015-04-22 22:29:11 +0000

    extended python wrapper classes HashMap and HashMapIndex

--- adt/_hashmap.py
+++ adt/_hashmap.py
@@ -2,7 +2,7 @@
 from ctypes  import *
 from _object import Object, Compound
 from _item   import Item
-from _ftypes import mappedType
+from _ftypes import MemoryRef, mappedType
 
 
 @mappedType( 'hashmap', 'type(HashMap_t)' )
@@ -34,8 +34,58 @@ class HashMap(Object):
     ptr = self.__getptr( self.get_, ident )
     ptr.contents.value = val
 
+  def __contains__( self, ident ):
+    return self.has_key_( byref(self), c_char_p(ident), c_int(len(ident)) ) != 0
+
+  def __iter__( self ):
+    for item in self.iteritems():
+      yield item[0]
+
+  def iteritems( self ):
+    idx = HashMapIndex()
+    self.index_( byref(idx), byref(self) )
+    while idx:
+      yield idx.get()
+      idx.next()
+
+  def iterkeys( self ):
+    for item in self.iteritems():
+      yield item[0]
+
+  def itervalues( self ):
+    for item in self.iteritems():
+      yield item[1]
+
+  def items( self ):
+    return list(self.iteritems())
+
+  def keys( self ):
+    return list(self.iterkeys())
+
+  def values( self ):
+    return list(self.itervalues())
+
+  def setdefault( self, ident, default = None ):
+    valPtr = POINTER(Item)()
+    self.set_default_( byref(valPtr), byref(self), c_char_p(ident), byref(Item(default)), c_int(len(ident)) )
+    return valPtr.contents
+
 
 
 class HashMapIndex(Compound):
-  pass
+
+  def __nonzero__( self ):
+    return self.is_valid_( byref(self) ) != 0
+
+  def get( self ):
+    keyRef, valPtr = MemoryRef(), POINTER(Item)()
+    dummy = "{0}".format(valPtr) # de-optimize Python (????), so next line works
+    self.item_( byref(keyRef), byref(valPtr), byref(self) )
+    return (str(keyRef), valPtr.contents)
+
+  def next( self ):
+    if self.set_next_( byref(self) ) == 0:
+      raise StopIteration
+  
+  
 
