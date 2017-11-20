53a207054dfa884d70a433e827d3d62211017d6d
Josef Scheuer <webseppl@web.de>
2017-11-20 14:10:28 +0000

    added missing _PROC_EXPORTS and entry in build configuration

--- file.f90
+++ file.f90
@@ -1,3 +1,4 @@
+#include "adt/itfUtil.fpp"
 
 #define _optArg(var, arg, default)       \
   if (present(arg)) then; var = arg     ;\
@@ -11,27 +12,28 @@
 
 module adt_file
   use adt_exception
-  
+
   contains
-  
+
+!_PROC_EXPORT(open_)
   subroutine open_( chnl, file, form, status, iostat, descr )
     integer                                         :: chnl
     character(len=*),                   intent(in)  :: file
     character(len=*), optional, target, intent(in)  :: form, status
     integer,          optional,         intent(out) :: iostat
     character(len=*), optional, target, intent(in)  :: descr
-  
+
     integer                                         :: iostat_
     character(len=512)                              :: buffer
-  
+
     character(10),                     save, target :: form_std   = 'FORMATTED' &
                                                      , status_std = 'UNKNOWN'   &
                                                      , descr_std  = 'file'
     character(:),                           pointer :: form_, status_, descr_
-  
+
     _optArgPtr(form_,   form,   form_std)
     _optArgPtr(status_, status, status_std)
-  
+
     close( chnl )
     open( chnl, file=file, form=form_, status=status_, iostat=iostat_, err=10 )
  10 if  (present(iostat)) then
@@ -45,22 +47,22 @@ module adt_file
     end if
  99 format( 'code ', I2, ' on opening ', A, ' ', A, ' in mode (', A, ', ', A, ')' )
   end subroutine
-  
-  
+
+!_PROC_EXPORT(open_jmp)
   subroutine open_jmp( chnl, *, file, form, status, iostat )
     integer                                         :: chnl
     character(len=*),                   intent(in)  :: file
     character(len=*), optional, target, intent(in)  :: form, status
     integer,          optional,         intent(out) :: iostat
     integer                                         :: iostat_
-  
+
     call open_( chnl, file, form, status, iostat_ )
     if (iostat_ /= 0) then; return 1
                       else; return
     end if
   end subroutine
-  
-  
+
+!_PROC_EXPORT(fopen)
   logical &
   function fopen( chnl, file, form, status, iostat )
     integer                                         :: chnl
@@ -68,20 +70,19 @@ module adt_file
     character(len=*), optional, target, intent(in)  :: form, status
     integer,          optional,         intent(out) :: iostat
     integer                                         :: iostat_
-    
+
     call open_( chnl, file, form, status, iostat_ )
     fopen = (iostat_ == 0)
   end function
-  
-  
+
+!_PROC_EXPORT(fopen_stat)
   integer &
   function fopen_stat( chnl, file, form, status, iostat )
     integer                                         :: chnl
     character(len=*),                   intent(in)  :: file
     character(len=*), optional, target, intent(in)  :: form, status
     integer,          optional,         intent(out) :: iostat
-    
+
     call open_( chnl, file, form, status, fopen_stat )
   end function
 end module
-
