092e6cd0439aff8ff900d5b63b47e1165d3742a1
Unknown <webseppl@web.de>
2018-11-29 23:21:10 +0100

    version-up to 2.7.2

--- fde/__init__.py
+++ fde/__init__.py
@@ -1,6 +1,6 @@
 
 __author__      = 'Josef Scheuer'
-__versioninfo__ = (2, 7, 1)
+__versioninfo__ = (2, 7, 2)
 __version__     = '.'.join( map( str, __versioninfo__ ) )
 __all__         = 'tools core control'.split()
 
