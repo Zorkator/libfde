8fe208cb3872db2b160fc683ef4aed8001dcd07e
Josef Scheuer <webseppl@web.de>
2019-04-11 00:03:20 +0200

    added function file_name
    some corrections in code formatting

--- src/file.f90
+++ src/file.f90
@@ -19,10 +19,11 @@ contains
 
 !_PROC_EXPORT(file_exists)
    logical &
-      function file_exists( file, iostat ) result(res)
+   function file_exists( file, iostat ) result(res)
       character(len=*),           intent(in)  :: file
       integer,          optional, intent(out) :: iostat
       integer                                 :: iostat_
+
       inquire( file=file, exist=res, iostat=iostat_ )
       if (present(iostat)) iostat = iostat_
    end function
@@ -30,10 +31,11 @@ contains
 
 !_PROC_EXPORT(file_opened_name)
    logical &
-      function file_opened_name( file, iostat ) result(res)
+   function file_opened_name( file, iostat ) result(res)
       character(len=*),           intent(in)  :: file
       integer,          optional, intent(out) :: iostat
       integer                                 :: iostat_
+
       inquire( file=file, opened=res, iostat=iostat_ )
       if (present(iostat)) iostat = iostat_
    end function
@@ -41,18 +43,33 @@ contains
 
 !_PROC_EXPORT(file_opened_unit)
    logical &
-      function file_opened_unit( unit, iostat ) result(res)
+   function file_opened_unit( unit, iostat ) result(res)
       integer,                    intent(in)  :: unit
       integer,          optional, intent(out) :: iostat
       integer                                 :: iostat_
+
       inquire( unit=unit, opened=res, iostat=iostat_ )
       if (present(iostat)) iostat = iostat_
    end function
 
 
+!_PROC_EXPORT(file_name)
+   function file_name( unit, fileBuff, iostat ) result(res)
+      integer,                    intent(in)  :: unit
+      character(len=*), target,   intent(out) :: fileBuff
+      integer,          optional, intent(out) :: iostat
+      integer                                 :: iostat_
+      character(len=:),              pointer  :: res
+
+      inquire( unit=unit, name=fileBuff, iostat=iostat_ )
+      if (present(iostat)) iostat = iostat_
+      res => fileBuff( : len_trim( fileBuff ) )
+   end function
+
+
 !_PROC_EXPORT(newunit)
    integer &
-      function newunit( unit, min, max )
+   function newunit( unit, min, max )
       integer, optional, intent(out) :: unit
       integer, optional, intent(in)  :: min, max
       integer                        :: unit_, min_, max_, ios
@@ -88,10 +105,10 @@ contains
       character(len=512)                              :: buffer
 
       character(10),                     save, target :: form_std   = 'FORMATTED'  &
-         , action_std = 'READWRITE'  &
-         , access_std = 'SEQUENTIAL' &
-         , status_std = 'UNKNOWN'    &
-         , descr_std  = 'file'
+                                                       , action_std = 'READWRITE'  &
+                                                       , access_std = 'SEQUENTIAL' &
+                                                       , status_std = 'UNKNOWN'    &
+                                                       , descr_std  = 'file'
       character(:),                           pointer :: form_, action_, access_, status_, descr_
       integer                                         :: recl_
       logical                                         :: reopened
@@ -104,7 +121,6 @@ contains
       _optArg(recl_,   recl,   1)
 
       reopened = file_opened( unit )
-
       if (.not. reopened) then
          if (lower(status_) == 'scratch') then
             open( unit,            form=form_, status=status_, action=action_, access=access_, recl=recl_, iostat=iostat_, err=10 )
@@ -121,15 +137,15 @@ contains
       elseif (iostat_ /= 0) then
          ! we handle error by throwing exception ...
          _optArgPtr(descr_, descr, descr_std)
-         if (reopened) then; write(buffer, 100, err=20) iostat_, trim(descr_), trim(file), trim(form_), trim(status_)
-         else; write(buffer, 200, err=20) iostat_, trim(descr_), trim(file), trim(form_), trim(status_)
+         if (reopened) then; write(buffer, 100, err=20) iostat_, trim(descr_), trim(file), unit, trim(form_), trim(status_)
+                       else; write(buffer, 200, err=20) iostat_, trim(descr_), trim(file), unit, trim(form_), trim(status_)
          end if
 
          20 call throw( IOError, buffer )
       end if
 
-      100 format( 'code ', I2, ', can not reopen ', A, ' "', A, '" in mode (', A, ', ', A, ')' )
-      200 format( 'code ', I2, ' on opening ', A, ' "', A, '" in mode (', A, ', ', A, ')' )
+      100 format( 'code ', I2, ', refuse reopening ', A, ' "', A, '" on channel ', I2, ' in mode (', A, ', ', A, ')' )
+      200 format( 'code ', I2, ', opening ',          A, ' "', A, '" on channel ', I2, ' in mode (', A, ', ', A, ')' )
    end subroutine
 
 
@@ -146,14 +162,14 @@ contains
       call open_( unit, file, form, status, action, access, recl, iostat_, descr )
       if (present(iostat)) iostat = iostat_
       if (iostat_ /= 0) then; return 1
-      else; return
+                        else; return
       end if
    end subroutine
 
 
 !_PROC_EXPORT(fopen_bool_)
    logical &
-      function fopen_bool_( unit, file, form, status, action, access, recl, iostat, descr ) result(res)
+   function fopen_bool_( unit, file, form, status, action, access, recl, iostat, descr ) result(res)
       integer,                            intent(in)  :: unit
       character(len=*),                   intent(in)  :: file
       character(len=*), optional, target, intent(in)  :: form, status, action, access
@@ -163,14 +179,14 @@ contains
 
       call open_( unit, file, form, status, action, access, recl, iostat, descr ) !< might throw IOError if iostat not given!
       if (present(iostat)) then; res = (iostat == 0) !< check for success: iostat = 0
-      else; res = .true.        !< there was no Exception - so it has to be success!
+                           else; res = .true.        !< there was no Exception - so it has to be success!
       end if
    end function
 
 
 !_PROC_EXPORT(fopen_autounit_)
    integer &
-      function fopen_autounit_( file, form, status, action, access, recl, iostat, descr ) result(res)
+   function fopen_autounit_( file, form, status, action, access, recl, iostat, descr ) result(res)
       character(len=*),                   intent(in)  :: file
       character(len=*), optional, target, intent(in)  :: form, status, action, access
       integer,          optional,         intent(in)  :: recl
@@ -193,7 +209,7 @@ contains
       character(len=256)                              :: file
 
       character(10),                     save, target :: status_std = 'KEEP' &
-         , descr_std  = 'file'
+                                                       , descr_std  = 'file'
       character(:),                           pointer :: status_, descr_
 
       _optArgPtr(status_, status, status_std)
@@ -206,10 +222,9 @@ contains
       elseif (iostat_ /= 0) then
          ! we handle error by throwing exception ...
          _optArgPtr(descr_, descr, descr_std)
-         inquire( unit, name=file )
-         write(buffer, 99, err=20) iostat_, trim(descr_), trim(file), trim(status_)
+         write(buffer, 99, err=20) iostat_, trim(descr_), file_name(unit, file), trim(status_)
 
-         20   call throw( IOError, buffer )
+         20 call throw( IOError, buffer )
       end if
 
       99 format( 'code ', I2, ' on closing ', A, ' "', A, '" with status ', A )
@@ -227,14 +242,14 @@ contains
       call close_( unit, status, iostat_, descr )
       if (present(iostat)) iostat = iostat_
       if (iostat_ /= 0) then; return 1
-      else; return
+                        else; return
       end if
    end subroutine
 
 
 !_PROC_EXPORT(fclose)
    logical &
-      function fclose( unit, status, iostat, descr ) result(res)
+   function fclose( unit, status, iostat, descr ) result(res)
       integer,                            intent(in)  :: unit
       character(len=*), optional, target, intent(in)  :: status
       integer,          optional,         intent(out) :: iostat
@@ -242,7 +257,7 @@ contains
 
       call close_( unit, status, iostat, descr )     !< might throw IOError if iostat not given!
       if (present(iostat)) then; res = (iostat == 0) !< check for success: iostat = 0
-      else; res = .true.        !< there was no Exception - so it has to be success!
+                           else; res = .true.        !< there was no Exception - so it has to be success!
       end if
    end function
 
