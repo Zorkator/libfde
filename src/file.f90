27e9f879723980ec9e35fc0dcc80a87e3e320a09
Josef Scheuer <webseppl@web.de>
2019-04-11 23:39:36 +0200

    added routines and interfaces fopen, fclose, open, close ...
    overloaded routines to accept arrays of file-names and units
    use gerror to get RT-message strings when throwing exceptions

--- src/file.f90
+++ src/file.f90
@@ -6,26 +6,67 @@ module fde_file
    implicit none
 
    interface fopen
-      module procedure fopen_bool_, fopen_autounit_
+      module procedure fopen_bool_, fopen_bool_v, fopen_autounit_, fopen_autounit_v
+   end interface
+
+   interface fclose
+      module procedure fclose_, fclose_v
+   end interface
+
+   interface file_exists
+      module procedure file_exists_, file_exists_v
    end interface
 
    interface file_opened
       module procedure file_opened_name, file_opened_unit
    end interface
 
+   interface open
+      module procedure open_, open_v
+   end interface
+
+   interface close
+      module procedure close_, close_v
+   end interface
+
    private :: fopen_bool_, fopen_autounit_
+
+   character(12), private, target :: access_opts(3)   = ['SEQUENTIAL', 'DIRECT', 'TRANSPARENT']
+   character(12), private, target :: action_opts(3)   = ['READWRITE', 'READ', 'WRITE']
+   character(12), private, target :: form_opts(3)     = ['FORMATTED', 'UNFORMATED', 'BINARY']
+   character(12), private, target :: position_opts(3) = ['ASIS', 'REWIND', 'APPEND']
+   character(12), private, target :: status_opts(5)   = ['UNKNOWN', 'NEW', 'REPLACE', 'SCRATCH', 'OLD']
+   character(12), private, target :: descr_opts(1)    = ['file']
+
+   character(16), private, target :: errBase_opts(2)  = ['opening', 'refuse reopening']
+
+#  define _open_kwArgs_list    access, action, form, position, status, descr
 
 contains
 
-!_PROC_EXPORT(file_exists)
+!_PROC_EXPORT(file_exists_)
    logical &
-   function file_exists( file, iostat ) result(res)
+   function file_exists_( file, iostat ) result(res)
       character(len=*),           intent(in)  :: file
       integer,          optional, intent(out) :: iostat
       integer                                 :: iostat_
 
       inquire( file=file, exist=res, iostat=iostat_ )
       if (present(iostat)) iostat = iostat_
+   end function
+
+
+!_PROC_EXPORT(file_exists_v)
+   function file_exists_v( file, iostat ) result(res)
+      character(len=*),           intent(in)  :: file(:)
+      integer,          optional, intent(out) :: iostat
+      integer                                 :: iostat_, i
+      logical                                 :: res(size(file))
+
+      do i = 1, size(file)
+         inquire( file=file(i), exist=res(i), iostat=iostat_ )
+         if (present(iostat)) iostat = max( iostat, iostat_ )
+      end do
    end function
 
 
@@ -92,74 +133,100 @@ contains
 
 
 !_PROC_EXPORT(open_)
-   subroutine open_( unit, file, form, status, action, access, recl, iostat, descr )
+   subroutine open_( unit, file, _open_kwArgs_list, recl, iostat )
+      _use_if_INTEL(ifcore)
       use fde_string
       integer,                            intent(in)  :: unit
       character(len=*),                   intent(in)  :: file
-      character(len=*), optional, target, intent(in)  :: form, status, action, access
+      character(len=*), optional, target, intent(in)  :: _open_kwArgs_list
       integer,          optional,         intent(in)  :: recl
       integer,          optional,         intent(out) :: iostat
-      character(len=*), optional, target, intent(in)  :: descr
-
       integer                                         :: iostat_
       character(len=512)                              :: buffer
+      character(:),                           pointer :: access_, action_, form_, position_, status_, descr_, errBase_
 
-      character(10),                     save, target :: form_std   = 'FORMATTED'  &
-                                                       , action_std = 'READWRITE'  &
-                                                       , access_std = 'SEQUENTIAL' &
-                                                       , status_std = 'UNKNOWN'    &
-                                                       , descr_std  = 'file'
-      character(:),                           pointer :: form_, action_, access_, status_, descr_
-      integer                                         :: recl_
-      logical                                         :: reopened
+      ! Set pointer to either given string argument or the default string ...
+      _optArgPtr( access_,   access,   access_opts(1) )
+      _optArgPtr( action_,   action,   action_opts(1) )
+      ! CAUTION: The default of FORM depends on chosen ACCESS!!
+      !          Thus, for FORM we use the same index where we find the chosen ACCESS in access_opts
+      _optArgPtr( form_,     form,     form_opts( indexOf(upper(access_), access_opts, 1) ) )
+      _optArgPtr( position_, position, position_opts(1) )
+      _optArgPtr( status_,   status,   status_opts(1) )
+      _optArgPtr( descr_,    descr,    descr_opts(1) )
 
-      _optArgPtr(form_,   form,   form_std)
-      _optArgPtr(status_, status, status_std)
-      _optArgPtr(action_, action, action_std)
-      _optArgPtr(access_, access, access_std)
-
-      _optArg(recl_,   recl,   1)
-
-      reopened = file_opened( unit )
-      if (.not. reopened) then
-         if (lower(status_) == 'scratch') then
-            open( unit,            form=form_, status=status_, action=action_, access=access_, recl=recl_, iostat=iostat_, err=10 )
-         else
-            open( unit, file=file, form=form_, status=status_, action=action_, access=access_, recl=recl_, iostat=iostat_, err=10 )
-         end if
+      if (file_opened( unit )) then
+         errBase_ => errBase_opts(2)
+         iostat_  =  34
       else
-         iostat_ = 14 !< there's only a generic open error code :-/
+         errBase_ => errBase_opts(1)
+
+#        define _open_kwArgs_pass   access=access_, action=action_, form=form_, position=position_, status=status_
+
+         if (upper(status_) == 'SCRATCH') then
+            if (present(recl)) then; open( unit,            _open_kwArgs_pass, recl=recl, iostat=iostat_, err=10 )
+                               else; open( unit,            _open_kwArgs_pass,            iostat=iostat_, err=10 )
+            end if
+         else
+            if (present(recl)) then; open( unit, file=file, _open_kwArgs_pass, recl=recl, iostat=iostat_, err=10 )
+                               else; open( unit, file=file, _open_kwArgs_pass,            iostat=iostat_, err=10 )
+            end if
+         end if
       end if
 
       10 if (present(iostat)) then
-         ! caller wants iostat ...
+         ! caller just wants iostat ...
          iostat = iostat_
-      elseif (iostat_ /= 0) then
-         ! we handle error by throwing exception ...
-         _optArgPtr(descr_, descr, descr_std)
-         if (reopened) then; write(buffer, 100, err=20) iostat_, trim(descr_), trim(file), unit, trim(form_), trim(status_)
-                       else; write(buffer, 200, err=20) iostat_, trim(descr_), trim(file), unit, trim(form_), trim(status_)
-         end if
 
+      elseif (iostat_ /= 0) then
+         ! otherwise we handle error by throwing an exception ...
+         write(buffer, 100, err=20) iostat_, trim(errBase_), trim(descr_), trim(file)
+         call gerror( buffer(len_trim(buffer)+2:) )
          20 call throw( IOError, buffer )
       end if
 
-      100 format( 'code ', I2, ', refuse reopening ', A, ' "', A, '" on channel ', I2, ' in mode (', A, ', ', A, ')' )
-      200 format( 'code ', I2, ', opening ',          A, ' "', A, '" on channel ', I2, ' in mode (', A, ', ', A, ')' )
+      100 format( 'code ', I3, ', ', A, ' ', A, ' "', A, '": ' )
+
+   contains
+
+      integer &
+      function indexOf( what, array, default ) result(idx)
+         character(len=*),               intent(in) :: what
+         character(len=*), dimension(:), intent(in) :: array
+         integer                                    :: default
+         do idx = 1, size(array)
+            if (what == array(idx)) return
+         end do
+         idx = default
+      end function
+   end subroutine
+
+
+!_PROC_EXPORT(open_v)
+   subroutine open_v( units, files, _open_kwArgs_list, recl, iostat )
+      integer,          dimension(:),           intent(in) :: units
+      character(len=*), dimension(size(units)), intent(in) :: files
+      character(len=*), optional,       target, intent(in) :: _open_kwArgs_list
+      integer,          optional,               intent(in) :: recl
+      integer,          optional,              intent(out) :: iostat
+      integer                                              :: i
+
+      do i = 1, size(units)
+         call open( units(i), files(i), _open_kwArgs_list, recl, iostat )
+      end do
    end subroutine
 
 
 !_PROC_EXPORT(open_jmp)
-   subroutine open_jmp( unit, *, file, form, status, action, access, recl, iostat, descr )
+   subroutine open_jmp( unit, *, file, _open_kwArgs_list, recl, iostat )
       integer,                            intent(in)  :: unit
       character(len=*),                   intent(in)  :: file
-      character(len=*), optional, target, intent(in)  :: form, status, action, access
+      character(len=*), optional, target, intent(in)  :: _open_kwArgs_list
       integer,          optional,         intent(in)  :: recl
       integer,          optional,         intent(out) :: iostat
-      character(len=*), optional, target, intent(in)  :: descr
       integer                                         :: iostat_
 
-      call open_( unit, file, form, status, action, access, recl, iostat_, descr )
+      call open_( unit, file, _open_kwArgs_list, recl, iostat_ )
       if (present(iostat)) iostat = iostat_
       if (iostat_ /= 0) then; return 1
                         else; return
@@ -169,77 +236,115 @@ contains
 
 !_PROC_EXPORT(fopen_bool_)
    logical &
-   function fopen_bool_( unit, file, form, status, action, access, recl, iostat, descr ) result(res)
+   function fopen_bool_( unit, file, _open_kwArgs_list, recl, iostat ) result(res)
       integer,                            intent(in)  :: unit
       character(len=*),                   intent(in)  :: file
-      character(len=*), optional, target, intent(in)  :: form, status, action, access
+      character(len=*), optional, target, intent(in)  :: _open_kwArgs_list
       integer,          optional,         intent(in)  :: recl
       integer,          optional,         intent(out) :: iostat
-      character(len=*), optional, target, intent(in)  :: descr
 
-      call open_( unit, file, form, status, action, access, recl, iostat, descr ) !< might throw IOError if iostat not given!
+      call open_( unit, file, _open_kwArgs_list, recl, iostat ) !< might throw IOError if iostat not given!
       if (present(iostat)) then; res = (iostat == 0) !< check for success: iostat = 0
                            else; res = .true.        !< there was no Exception - so it has to be success!
       end if
    end function
 
 
+!_PROC_EXPORT(fopen_bool_v)
+   function fopen_bool_v( units, files, _open_kwArgs_list, recl, iostat ) result(res)
+      integer,          dimension(:),           intent(in)  :: units
+      character(len=*), dimension(size(units)), intent(in)  :: files
+      character(len=*), optional, target,       intent(in)  :: _open_kwArgs_list
+      integer,          optional,               intent(in)  :: recl
+      integer,          optional,               intent(out) :: iostat
+      logical,          dimension(size(files))              :: res
+      integer                                               :: i
+
+      do i = 1, size(files)
+         res(i) = fopen( units(i), files(i), _open_kwArgs_list, recl, iostat )
+      end do
+   end function
+
+
 !_PROC_EXPORT(fopen_autounit_)
    integer &
-   function fopen_autounit_( file, form, status, action, access, recl, iostat, descr ) result(res)
+   function fopen_autounit_( file, _open_kwArgs_list, recl, iostat ) result(res)
       character(len=*),                   intent(in)  :: file
-      character(len=*), optional, target, intent(in)  :: form, status, action, access
+      character(len=*), optional, target, intent(in)  :: _open_kwArgs_list
       integer,          optional,         intent(in)  :: recl
       integer,          optional,         intent(out) :: iostat
-      character(len=*), optional, target, intent(in)  :: descr
 
-      call open_( newunit(res), file, form, status, action, access, recl, iostat, descr )
+      call open_( newunit(res), file, _open_kwArgs_list, recl, iostat )
+   end function
+
+
+!_PROC_EXPORT(fopen_autounit_v)
+   function fopen_autounit_v( files, _open_kwArgs_list, recl, iostat ) result(res)
+      character(len=*), dimension(:),     intent(in)  :: files
+      character(len=*), optional, target, intent(in)  :: _open_kwArgs_list
+      integer,          optional,         intent(in)  :: recl
+      integer,          optional,         intent(out) :: iostat
+      integer,          dimension(size(files))        :: res
+      integer                                         :: i
+
+      do i = 1, size(files)
+         res(i) = fopen( files(i), _open_kwArgs_list, recl, iostat )
+      end do
    end function
 
 
 !_PROC_EXPORT(close_)
-   subroutine close_( unit, status, iostat, descr )
+   subroutine close_( unit, status, descr, iostat )
+      _use_if_INTEL(ifcore)
       integer,                            intent(in)  :: unit
-      character(len=*), optional, target, intent(in)  :: status
+      character(len=*), optional, target, intent(in)  :: status, descr
       integer,          optional,         intent(out) :: iostat
-      character(len=*), optional, target, intent(in)  :: descr
-
       integer                                         :: iostat_
       character(len=512)                              :: buffer
       character(len=256)                              :: file
+      character(:),                           pointer :: descr_
 
-      character(10),                     save, target :: status_std = 'KEEP' &
-                                                       , descr_std  = 'file'
-      character(:),                           pointer :: status_, descr_
-
-      _optArgPtr(status_, status, status_std)
-
-      close( unit, status=status_, iostat=iostat_, err=10 )
+      if (present(status)) then; close( unit, status=status, iostat=iostat_, err=10 )
+                           else; close( unit,                iostat=iostat_, err=10 )
+      end if
 
       10 if (present(iostat)) then
          ! caller wants iostat ...
          iostat = iostat_
-      elseif (iostat_ /= 0) then
-         ! we handle error by throwing exception ...
-         _optArgPtr(descr_, descr, descr_std)
-         write(buffer, 99, err=20) iostat_, trim(descr_), file_name(unit, file), trim(status_)
 
+      elseif (iostat_ /= 0) then
+         ! otherwise we handle error by throwing an exception ...
+         _optArgPtr(descr_, descr, descr_opts(1))
+         write(buffer, 100, err=20) iostat_, trim(descr_), file_name(unit, file)
+         call gerror( buffer(len_trim(buffer)+2:) )
          20 call throw( IOError, buffer )
       end if
 
-      99 format( 'code ', I2, ' on closing ', A, ' "', A, '" with status ', A )
+      100 format( 'code ', I3, ', on closing ', A, ' "', A, '": ' )
+   end subroutine
+
+
+!_PROC_EXPORT(close_v)
+   subroutine close_v( units, status, descr, iostat )
+      integer,          dimension(:), intent(in)  :: units
+      character(len=*), optional,     intent(in)  :: status, descr
+      integer,          optional,     intent(out) :: iostat
+      integer                                     :: i
+
+      do i = 1, size(units)
+         call close_( units(i), status, descr, iostat )
+      end do
    end subroutine
 
 
 !_PROC_EXPORT(close_jmp)
-   subroutine close_jmp( unit, *, status, iostat, descr )
-      integer,                            intent(in)  :: unit
-      character(len=*), optional, target, intent(in)  :: status
-      integer,          optional,         intent(out) :: iostat
-      character(len=*), optional, target, intent(in)  :: descr
-      integer                                         :: iostat_
+   subroutine close_jmp( unit, *, status, descr, iostat )
+      integer,                    intent(in)  :: unit
+      character(len=*), optional, intent(in)  :: status, descr
+      integer,          optional, intent(out) :: iostat
+      integer                                 :: iostat_
 
-      call close_( unit, status, iostat_, descr )
+      call close_( unit, status, descr, iostat_ )
       if (present(iostat)) iostat = iostat_
       if (iostat_ /= 0) then; return 1
                         else; return
@@ -247,18 +352,31 @@ contains
    end subroutine
 
 
-!_PROC_EXPORT(fclose)
+!_PROC_EXPORT(fclose_)
    logical &
-   function fclose( unit, status, iostat, descr ) result(res)
-      integer,                            intent(in)  :: unit
-      character(len=*), optional, target, intent(in)  :: status
-      integer,          optional,         intent(out) :: iostat
-      character(len=*), optional, target, intent(in)  :: descr
+   function fclose_( unit, status, descr, iostat ) result(res)
+      integer,                    intent(in)  :: unit
+      character(len=*), optional, intent(in)  :: status, descr
+      integer,          optional, intent(out) :: iostat
 
-      call close_( unit, status, iostat, descr )     !< might throw IOError if iostat not given!
+      call close_( unit, status, descr, iostat )     !< might throw IOError if iostat not given!
       if (present(iostat)) then; res = (iostat == 0) !< check for success: iostat = 0
                            else; res = .true.        !< there was no Exception - so it has to be success!
       end if
+   end function
+
+
+!_PROC_EXPORT(fclose_v)
+   function fclose_v( units, status, descr, iostat ) result(res)
+      integer,          dimension(:), intent(in)  :: units
+      character(len=*), optional,     intent(in)  :: status, descr
+      integer,          optional,     intent(out) :: iostat
+      logical,          dimension(size(units))    :: res
+      integer                                     :: i
+
+      do i = 1, size(units)
+         res(i) = fclose( units(i), status, descr, iostat )
+      end do
    end function
 
 end module
