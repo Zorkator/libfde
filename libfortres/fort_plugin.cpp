Zorkator/libfde

commit 014aea07cc9d12193ece437075ce0c278cd51181

added initial draft for routines plugin_iterate_so

libfortres/fort_plugin.cpp
@@ -15,6 +15,7 @@
 
 #if defined HAVE_DLFCN_H
 # include <dlfcn.h>
+# include <link.h>
 
 # define dlOpen(lib)        dlopen( lib, RTLD_NOW | RTLD_GLOBAL )
 # define dlClose(hdl)       dlclose( hdl )
@@ -25,6 +26,7 @@
 #elif defined HAVE_LIBLOADERAPI_H
 # include <windows.h>
 # include <libloaderapi.h>
+# include <psapi.h>
 
 # define dlOpen(lib)        LoadLibraryA( (LPCSTR)lib )
 # define dlClose(hdl)       FreeLibrary( (HMODULE)hdl )
@@ -482,5 +484,57 @@ f_plugin_try_call( StringRef *pluginId, StringRef *symId )
   if (func != NULL)
     { reinterpret_cast<SharedLib::Function>(func)(); }
   return (func != NULL);
+}
+
+
+_dllExport_C
+void
+f_plugin_iterate_so( SOInfoHandler handler )
+{
+  StringRef moduleId;
+
+#if defined HAVE_WINDOWS_H
+  HMODULE      handles[1024];
+  HANDLE       procHandle;
+  DWORD        numBytes;
+  std::wstring idBuffer;
+  idBuffer.reserve( MAX_PATH );
+
+
+  procHandle = GetCurrentProcess();
+  if (EnumProcessModules( procHandle, handles, sizeof(handles), &numBytes ))
+  {
+    for (unsigned int i = 0; i < (numBytes / sizeof(HMODULE)); ++i)
+    {
+      if (GetModuleFileNameEx( procHandle, handles[i], (TCHAR *)&idBuffer[0], idBuffer.capacity() ))
+      {
+        moduleId.referTo( (const char *)idBuffer.data(), idBuffer.length() );
+        handler( &moduleId, handles[i] );
+      }
+    }
+  }
+  CloseHandle( procHandle );
+
+#else
+  struct CB_Data
+  {
+    StringRef     &_moduleId;
+    SOInfoHandler &_handler;
+  } DATA = {moduleId, handler};
+
+  struct CB_Code
+  {
+    static int
+    callback( struct dl_phdr_info *info, size_t size, void *_data )
+    {
+      CB_Data &data = *static_cast<CB_Data *>(_data);
+      data._moduleId.referTo( info->dlpi_name );
+      data._handler( &data._moduleId, (void *)&info->dlpi_addr );
+      return 0;
+    }
+  };
+
+  dl_iterate_phdr( CB_Code::callback, &DATA );
+#endif
 }
 

test/CMakeLists.txt
@@ -54,4 +54,5 @@ new_test(
 #./test_simulator.F90
 #./test_simulator_driver.F90
 ./test_string.F90
+./test_plugin.F90
 )

libfortres/fort_dirent.cpp
@@ -18,9 +18,6 @@
   /* assume POSIX compatible compiler */
 # include <stdlib.h>
 # include <unistd.h>
-# ifndef MAX_PATH
-#   define MAX_PATH   4096
-# endif
 #endif
 
 #if defined HAVE_DLFCN_H

libfortres/include/fortres/dirent.hpp
@@ -9,6 +9,10 @@
   /* assume POSIX compatible compiler */
 # include <dirent.h>
 
+# ifndef MAX_PATH
+#   define MAX_PATH    4096
+# endif
+
 #else
   /* sorry, it's M$ ... */
 # include <windows.h>

libfortres/include/fortres/plugin.fmod
@@ -14,6 +14,7 @@
         public :: plugin_iterate, plugin_filePath_to_id
         public :: plugin_available, plugin_sym, plugin_proc, plugin_call
         public :: plugin_try_sym, plugin_try_proc, plugin_try_call
+        public :: plugin_iterate_so
 
         interface
           subroutine PluginInfoHandler( id, fp, st )
@@ -22,6 +23,12 @@
             integer           :: st
           end subroutine
       
+          subroutine SOInfoHandler( id, addr )
+            import StringRef_t
+            type(StringRef_t) :: id
+            integer           :: addr
+          end subroutine
+
           subroutine plugin_set_path_( p, l, c ) _cID(f_plugin_set_path)
             import StringRef_t
             type(StringRef_t), intent(in) :: p !< plugin path
@@ -111,6 +118,11 @@
             type(StringRef_t), intent(in) :: sId !< symbolId
             integer(kind=c_int)           :: plugin_try_call_
           end function
+
+          subroutine plugin_iterate_so_( op ) _cID(f_plugin_iterate_so)
+            import c_funptr
+            type(c_funptr), value, intent(in) :: op
+          end subroutine
         end interface
 
       contains
@@ -221,6 +233,12 @@
             logical          :: res
             res = (plugin_try_call_( str(pluginId), str(symId) ) /= 0)
           end function
+
+!_PROC_EXPORT(plugin_iterate_so)
+          subroutine plugin_iterate_so( op )
+            procedure(SOInfoHandler) :: op
+            call plugin_iterate_so_( c_funloc(op) )
+          end subroutine
 
       end module
 

libfortres/include/fortres/plugin.hpp
@@ -5,6 +5,7 @@
 #include "fortres/portdef.h"
 
 typedef void (*PluginInfoHandler)( StringRef *id, StringRef *filePath, int *state );
+typedef void (*SOInfoHandler)( StringRef *id, const void *handle );
 
 _dllExport_C
 void f_plugin_set_path( StringRef *path, StringRef *libPath, StringRef *chkSym );
@@ -38,6 +39,9 @@ void * f_plugin_try_sym( StringRef *pluginId, StringRef *symId );
 
 _dllExport_C
 int f_plugin_try_call( StringRef *pluginId, StringRef *symId );
+
+_dllExport_C
+void f_plugin_iterate_so( SOInfoHandler handler );
 
 #endif /* __FORTRES_SHAREDLIB__HPP */
 

test/test_plugin.F90
@@ -1,0 +1,27 @@
+
+module test_plugin
+  use fde_plugin
+  use fde_convert
+  use fde_stringref
+  implicit none
+
+  contains
+
+  subroutine test_list_so()
+    call plugin_iterate_so( printSO )
+  end subroutine
+
+  subroutine printSO( id, addr )
+    type(StringRef_t) :: id
+    integer           :: addr
+    print*, hex(addr), ": ", str(id)
+  end subroutine
+
+end module
+
+program plugintest
+  use test_plugin 
+
+  call test_list_so()
+end program
+

commit f6993c1fcd4b6cbd04d90cbcbdba9accacc2e944

Cygwin compatibility was broken after 014aea07

libfortres/CMakeLists.txt
@@ -20,10 +20,13 @@ if(HAVE_WINDOWS_H)
   check_symbol_exists("strtok_s" "string.h" HAVE_STRING_SECURE)
   check_include_files("windows.h;DbgHelp.h" HAVE_DBGHELP_H)
   check_include_files("windows.h;libloaderapi.h" HAVE_LIBLOADERAPI_H)
+  check_include_files("windows.h;psapi.h" HAVE_PSAPI_H)
 endif()
 
 check_include_files("dirent.h" HAVE_DIRENT_H)
 check_include_files("dlfcn.h" HAVE_DLFCN_H)
+check_include_files("link.h" HAVE_LINK_H)
+
 
 if(HAVE_DBGHELP_H)
   target_link_libraries(fortres PRIVATE Dbghelp)
@@ -35,6 +38,10 @@ endif()
 
 if(HAVE_DLFCN_H)
   target_link_libraries(fortres PRIVATE dl)
+endif()
+
+if(HAVE_PSAPI_H)
+  target_link_libraries(fortres PRIVATE psapi)
 endif()
 
 #file(TO_NATIVE_PATH "/" LIB_DIR_SEP)

libfortres/fort_plugin.cpp
@@ -13,9 +13,12 @@
 #include "fortres/String.hpp"
 #include "fortres/stdlib.h"
 
+#if defined HAVE_WINDOWS_H
+# include <windows.h>
+#endif
+
 #if defined HAVE_DLFCN_H
 # include <dlfcn.h>
-# include <link.h>
 
 # define dlOpen(lib)        dlopen( lib, RTLD_NOW | RTLD_GLOBAL )
 # define dlClose(hdl)       dlclose( hdl )
@@ -24,9 +27,7 @@
 # define dlErrorCode()      errno
 
 #elif defined HAVE_LIBLOADERAPI_H
-# include <windows.h>
 # include <libloaderapi.h>
-# include <psapi.h>
 
 # define dlOpen(lib)        LoadLibraryA( (LPCSTR)lib )
 # define dlClose(hdl)       FreeLibrary( (HMODULE)hdl )
@@ -36,6 +37,14 @@
 
 #else
   #error "Neither HAVE_LIBLOADERAPI_H nor HAVE_DLFCN_H"
+#endif
+
+#if defined HAVE_LINK_H
+# include <link.h>
+#endif
+
+#if defined HAVE_PSAPI_H
+# include <psapi.h>
 #endif
 
 # define _ref_str(strRef)       strRef->str()

libfortres/fortres_config.h.in
@@ -2,9 +2,11 @@
 #cmakedefine HAVE_STRING_SECURE 1
 #cmakedefine HAVE_DBGHELP_H 1
 #cmakedefine HAVE_LIBLOADERAPI_H 1
+#cmakedefine HAVE_PSAPI_H 1
 
 #cmakedefine HAVE_DIRENT_H 1
 #cmakedefine HAVE_DLFCN_H 1
+#cmakedefine HAVE_LINK_H 1
 
 
 #define LIB_PREFIX      "@CMAKE_SHARED_LIBRARY_PREFIX@"