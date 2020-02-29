a3c493860afb9d700cffe190075bf1eac0e34f2f
Volker Jacht <kooky089@gmail.com>
2020-02-29 09:44:59 +0100

    Updated config to stay backwards compatible with gnu make

--- libfortres/config/fortres_config.h
+++ libfortres/config/fortres_config.h
@@ -3,16 +3,22 @@
 #define HAVE_STRING_SECURE 1
 #define HAVE_DBGHELP_H 1
 #define HAVE_LIBLOADERAPI_H 1
+#define HAVE_PSAPI_H 1
 
 /* #undef HAVE_DIRENT_H */
 /* #undef HAVE_DLFCN_H */
+/* #undef HAVE_LINK_H */
 
-#define LIB_PREFIX      ""
-#define LIB_SUFFIX      ".dll"
-#define LIB_PATH_VAR    "PATH"
-#define LIB_PATH_SEP    ";"
-#define PATH_SEP        "/"
-#define PATH_SEP_OTHER  "\\"
+
+#define LIB_PREFIX        ""
+#define LIB_SUFFIX        ".dll"
+#define LIB_PATH_VAR      "PATH"
+#define LIB_PATH_SEP      ";"
+#define PATH_SEP          "\\"
+#define PATH_SEP_UNIX     "/"
+#define PATH_SEP_WINDOWS  "\\"
+
+
 
 #else
 
@@ -20,16 +26,20 @@
 /* #undef HAVE_STRING_SECURE */
 /* #undef HAVE_DBGHELP_H */
 /* #undef HAVE_LIBLOADERAPI_H */
+/* #undef HAVE_PSAPI_H */
 
 #define HAVE_DIRENT_H 1
 #define HAVE_DLFCN_H 1
+#define HAVE_LINK_H 1
 
 
-#define LIB_PREFIX      "lib"
-#define LIB_SUFFIX      ".so"
-#define LIB_PATH_VAR    "LD_LIBRARY_PATH"
-#define LIB_PATH_SEP    ":"
-#define PATH_SEP        "/"
-#define PATH_SEP_OTHER  "\\"
+#define LIB_PREFIX        "lib"
+#define LIB_SUFFIX        ".so"
+#define LIB_PATH_VAR      "LD_LIBRARY_PATH"
+#define LIB_PATH_SEP      ":"
+#define PATH_SEP          "/"
+#define PATH_SEP_UNIX     "/"
+#define PATH_SEP_WINDOWS  "\\"
+
 
 #endif
