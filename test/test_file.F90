320ab78fb5da7f24fc4b842811846d5aca8fbbea
Josef Scheuer <webseppl@web.de>
2020-03-31 18:36:46 +0200

    added asserts to test_file

--- test/test_file.F90
+++ test/test_file.F90
@@ -1,6 +1,9 @@
 
+#include "fde/exception.fpp"
+
 module test_file
   use fde_file
+  use fde_exception
   implicit none
 
   character(len=255) :: buffer
@@ -12,15 +15,21 @@ contains
     character(10) :: files(5) = ["test.1.txt", "test.2.txt", "test.3.txt", "test.4.txt", "test.5.txt"]
 
     fh = fopen( files, position='append' )
-    print *, file_exists( files )
+    _assert( file_exists( files ) )
 
     do i = 1, size(files)
-      write(*, *) i, file_name( fh(i), buffer )
+      _assert( file_name( fh(i), buffer ) == files(i) )
     end do
 
     call close( fh, status="delete" )
-    print *, file_exists( files )
-    print *, file_size( __FILE__ )
+    _assert( file_exists( files ) == .false. )
+
+#   define _testfile 'size_test.bj'
+
+    fh(1) = fopen( _testfile, form='UNFORMATTED' )
+    write( fh(1), * ) 42
+    call close( fh(1) )
+    _assert( file_size( _testfile ) == 12 )
   end subroutine
 
 end module
