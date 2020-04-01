ca26883bd382d90913cbc86ac36b7e1a861ab42b
Josef Scheuer <webseppl@web.de>
2020-04-01 12:20:30 +0200

    use assertions in test_file
    fixed logical comparison for gfortran

--- test/test_file.F90
+++ test/test_file.F90
@@ -3,6 +3,7 @@
 
 module test_file
   use fde_file
+  use fde_string
   use fde_exception
   implicit none
 
@@ -15,27 +16,31 @@ contains
     character(10) :: files(5) = ["test.1.txt", "test.2.txt", "test.3.txt", "test.4.txt", "test.5.txt"]
 
     fh = fopen( files, position='append' )
-    _assert( file_exists( files ) )
+    _assert( all( file_exists( files ) ) )
 
     do i = 1, size(files)
-      _assert( file_name( fh(i), buffer ) == files(i) )
+      _assert( filename( file_name( fh(i), buffer ) ) == files(i) )
     end do
 
     call close( fh, status="delete" )
-    _assert( file_exists( files ) == .false. )
+    _assert( all( file_exists( files ) .eqv. .false. ) )
 
 #   define _testfile 'size_test.bj'
 
-    fh(1) = fopen( _testfile, form='UNFORMATTED' )
-    write( fh(1), * ) 42
+    fh(1) = fopen( _testfile, form='UNFORMATTED', action='WRITE' )
+    write( fh(1) ) 42
     call close( fh(1) )
+    _assert( file_exists( _testfile ) )
     _assert( file_size( _testfile ) == 12 )
+
+    call close( fopen( _testfile ), status="delete" )
+    _assert( .not. file_exists( _testfile ) )
   end subroutine
 
 end module
 
 program testing
-  use test_file 
+  use test_file
 
   call test_open_close()
 end program
