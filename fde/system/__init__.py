9887506e9c69e50d4426302d12f2de168e4a29a0
Josef Scheuer <josef.scheuer@grs.de>
2022-08-04 10:34:35 +0200

    fde.system: added package for fde-based systems/controllables (e.g. simulators) in python

---
+++ fde/system/__init__.py
@@ -0,0 +1,7 @@
+
+__author__      = 'Josef Scheuer'
+__versioninfo__ = (2, 8, 0)
+__version__     = '.'.join( map( str, __versioninfo__ ) )
+__all__         = []
+
+from ._simulator import Simulator
