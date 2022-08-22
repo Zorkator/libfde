1d716216cd7eb1cb3acd1ac110ea05fd2e9a9361
Josef Scheuer <josef.scheuer@grs.de>
2022-08-22 18:25:37 +0200

    control.CommandProcessor: use actionContext for evaluation/execution of command strings

--- fde/control/_commandProcessor.py
+++ fde/control/_commandProcessor.py
@@ -1,54 +1,68 @@
 
-from traceback import format_exception
-from six       import string_types
-import sys, os, shlex
+from ._stateful import cached_property
+from traceback  import format_exception
+import sys, os
 
 #--------------------------------------------
 class CommandProcessor( object ):
 #--------------------------------------------
-    """Abstract Mixin class extending Startable, Stateful Controller types.
+    """Mixin class extending Startable, Stateful Controller types.
     Used interfaces:
       Stateful : setData, getData, makeKeyTokenizer, state
       Startable: fork
 
-    CommandProcessor provides a simple interface for executing the main loop of Startable codes
-      controlled by commands.
+    CommandProcessor provides a simple command loop for executing Startable and Stateful
+      codes interactively.
     """
+    _prompt = '>>> '
+    CMD_OK  = 0xC0DE0
 
     def __init__( self, *args, **kwArgs ):
         super(CommandProcessor, self).__init__( *args, **kwArgs )
         self._doProcess = True
+        self._ticks     = []
 
 
     def processCommands( self ):
         while self._doProcess:
+            tick, self._ticks[:] = self._ticks[:1], self._ticks[1:]
+            if tick:
+                break
+            # ... no pending ticks ...
             try:
-                res = None
                 cmd = self.receive()
                 if self.opts.debug > 0:
                     from ..tools import debug; debug()
-
-                if   isinstance( cmd, string_types ): res = self._dispatchCommand( cmd )
-                elif hasattr( cmd, 'keys' )         : res = self.setData( cmd )
-                elif hasattr( cmd, '__iter__' )     : res = self.getData( cmd )
-                else                                : res = "unknown command"
-
-            except StopIteration:
-                res = 'ok'
-                break
-
+                if   hasattr( cmd, 'strip' )   : res = self.evalCommand( cmd )
+                elif hasattr( cmd, 'keys' )    : res = self.setData( cmd )
+                elif hasattr( cmd, '__iter__' ): res = self.getData( cmd )
+                else                           : res = "unknown command"
             except Exception as e:
                 res = e.__class__( ''.join( format_exception( *sys.exc_info() ) ) )
-
             finally:
                 self.send( res )
 
 
-    def _dispatchCommand( self, cmd ):
-        cmd = shlex.split( cmd )
-        res = getattr( self, "cmd_" + cmd[0] )( *cmd[1:] )
-        if res is None: raise StopIteration
-        else          : return res
+    def evalCommand( self, cmd ):
+        ctxt = self.actionContext
+        try:
+            return ctxt.eval( cmd )
+        except SyntaxError:
+            ctxt.exec( cmd )
+            return self.CMD_OK
+
+
+    @cached_property
+    def actionContext( self ):
+        """return default ActionContext object, using class types for Action, Trigger and VariableLookup."""
+        context  = super(CommandProcessor, self).actionContext
+        selfType = type(self)
+        members  = [ getattr( selfType, m ) for m in dir( selfType ) if m.startswith( 'cmd_' ) ]
+        commands = [ getattr( f, 'fget', f ) for f in members ] #< treat cmd-properties the same
+        context.globals.update(
+            (cmd.__name__[4:], cmd.__get__(self)) for cmd in commands
+        )
+        return context
 
 
     ####
@@ -57,7 +71,6 @@ class CommandProcessor( object ):
 
     def cmd_setKeyOp( self, sep = None ):
         self.keyTokenizer = self.makeKeyTokenizer( sep )
-        return True
 
     # Some default commands for Simulators.
     # Subclasses might need to reimplement these.
@@ -66,7 +79,7 @@ class CommandProcessor( object ):
     def cmd_state( self )    : return self.state
     def cmd_getcwd( self )   : return os.getcwd()
 
-    def cmd_tick( self )     : pass
+    def cmd_tick( self, n=1 ): self._ticks.extend( (1,) * n )
     def cmd_finalize( self ) : self._doProcess = False
     def cmd_terminate( self ): self._doProcess = False
 
@@ -75,13 +88,12 @@ class CommandProcessor( object ):
 
     def cmd_debug( self, stat ):
         self.opts.debug = int( stat.lower() in 'on true 1 yes enabled'.split() )
-        return True
-
 
     # methods to be [re-]implemented by subclasses
 
     def receive( self ):
-        raise NotImplementedError
+        return input( self._prompt )
 
     def send( self, what ):
-        raise NotImplementedError
+        if what is not self.CMD_OK:
+            print( repr(what) )
