fdab19f23d62b048a8c9044368c3038ac7f0c2f9
Josef Scheuer <josef.scheuer@grs.de>
2024-01-18 11:19:11 +0100

    split off BaseCommandProcessor as CommandProcessor without ticks

--- fde/control/__init__.py
+++ fde/control/__init__.py
@@ -4,23 +4,24 @@ __versioninfo__ = (2, 8, 2)
 __version__     = '.'.join( map( str, __versioninfo__ ) )
 __all__         = []
 
-from ._controllable       import Controllable
-from ._fdeControllable    import FDEControllable
-from ._exceptionRouter    import ExceptionRouter
-from ._stateful           import Stateful
-from ._hookable           import Hookable, connect_to_hook
-from ._startable          import Startable
-from ._fdeStartable       import FDEStartable
-from ._pyStartable        import PyStartable
-from ._verbose            import Verbose
-from ._commandProcessor   import CommandProcessor
-from ._fdeSimulator       import Simulator, FDESimulator
-from ._pySimulator        import PySimulator
-from ._expression         import Expression
-from ._actionContext      import ActionContextHost, ActionContext, Trigger, Action
-from ._variable           import Variable
+from ._controllable         import Controllable
+from ._fdeControllable      import FDEControllable
+from ._exceptionRouter      import ExceptionRouter
+from ._stateful             import Stateful
+from ._hookable             import Hookable, connect_to_hook
+from ._startable            import Startable
+from ._fdeStartable         import FDEStartable
+from ._pyStartable          import PyStartable
+from ._verbose              import Verbose
+from ._baseCommandProcessor import BaseCommandProcessor
+from ._commandProcessor     import CommandProcessor
+from ._fdeSimulator         import Simulator, FDESimulator
+from ._pySimulator          import PySimulator
+from ._expression           import Expression
+from ._actionContext        import ActionContextHost, ActionContext, Trigger, Action
+from ._variable             import Variable
 
 __all__.extend( 'Controllable FDEControllable ExceptionRouter'.split() )
-__all__.extend( 'Stateful Hookable connect_to_hook Startable FDEStartable Verbose CommandProcessor'.split() )
+__all__.extend( 'Stateful Hookable connect_to_hook Startable FDEStartable Verbose BaseCommandProcessor CommandProcessor'.split() )
 __all__.extend( 'Simulator FDESimulator PySimulator'.split() )
 __all__.extend( 'Expression ActionContextHost ActionContext Trigger Action Variable'.split() )
---
+++ fde/control/_baseCommandProcessor.py
@@ -0,0 +1,91 @@
+
+from traceback       import format_exception
+from ._actionContext import ActionContextHost
+
+#--------------------------------------------------
+class BaseCommandProcessor( ActionContextHost ):
+#--------------------------------------------------
+    """Mixin class extending Startable, Stateful Controller types.
+    BaseCommandProcessor provides a simple command loop for executing Stateful
+      codes interactively.
+    """
+    _prompt = '>>> '
+
+    def __init__( self, *args, **kwArgs ):
+        super(BaseCommandProcessor, self).__init__( *args, **kwArgs )
+        self._doProcess = True
+        self._loop      = True
+        self._ticks     = []
+
+
+    def processCommands( self ):
+        self._loop = True
+        while self._doProcess and self._loop:
+            self.do_commands()
+
+
+    def do_commands( self ):
+        try:
+            cmd = self.receive()
+            if self.opts.debug > 0:
+                from ..tools import debug; debug()
+            if   hasattr( cmd, 'strip' )   : res = self.evalCommand( cmd )
+            elif hasattr( cmd, 'keys' )    : res = self.setData( cmd )
+            elif hasattr( cmd, '__iter__' ): res = self.getData( cmd )
+            else                           : res = "unknown command"
+        except Exception as e:
+            res = e
+        finally:
+            self.send( res )
+
+
+    def evalCommand( self, cmd ):
+        ctxt = self.actionContext
+        try:
+            return ctxt.eval_code( cmd )
+        except SyntaxError:
+            ctxt.exec_code( cmd )
+
+
+    def makeActionContext( self, varLookup = None, cmdPrefix = 'cmd_' ):
+        return super(BaseCommandProcessor, self).makeActionContext( varLookup, cmdPrefix )
+
+    ####
+    # command implementations
+    #
+
+    def cmd_exit( self ):
+        """disable command loop and continue execution of Startable."""
+        self._doProcess = False
+
+    def cmd_globals( self ):
+        """return dictionary of global definitions."""
+        return self.actionContext.globals
+
+    def cmd_locals( self ):
+        """return dictionary of global definitions."""
+        return self.actionContext.locals
+
+    # methods that might be reimplemented by subclasses
+
+    def receive( self ):
+        try:
+            return self.__receive__()
+        except EOFError:
+            return 'exit()' #< just exit on closed stdin!
+
+    def send( self, what ):
+        if what is not None:
+            if isinstance( what, Exception ):
+                what = ''.join( format_exception( type(what), what, what.__traceback__ ) )
+            self.__send__( what )
+
+    def __receive__( self ):
+        return input( self._prompt )
+
+    def __send__( self, what ):
+        print( what )
+
+
+
+#-------------------------------------------------
