7ebe5f8761d86e1135402821a84eb0ac61d51a79
Josef Scheuer <josef.scheuer@grs.de>
2024-12-20 19:38:54 +0100

    control: instanciate Deferred-subtypes explicitly

--- fde/control/_expression.py
+++ fde/control/_expression.py
@@ -72,7 +72,7 @@ class DeferredType( type(ContextEvaluable) ):
 #-----------------------------------------------
 
     def eval( _class, ident: str ):
-        return Deferred( ident, _eval=(_class._globals, _class._locals) )
+        return DeferredEval( ident, (_class._globals, _class._locals) )
 
     def wrap( _class, obj ):
         if isinstance( obj, str ): return _class.eval( obj )
@@ -82,48 +82,31 @@ class DeferredType( type(ContextEvaluable) ):
 #-------------------------------------------------------------
 class Deferred( ContextEvaluable, metaclass=DeferredType ):
 #-------------------------------------------------------------
-    __slots__ = ('_obj',)
-    _attr = ''
-    _args = None
-    _item = None
-    _eval = None
+    __slots__ = ('_obj', '_data')
 
-    def __init__( self, obj, **kwArgs ):
-        self._obj = obj
-        vars(self).update( kwArgs )
+    def __init__( self, obj, data = None ):
+        self._obj  = obj
+        self._data = data
 
     def __getattr__( self, ident ):
-        return type(self)( self, _attr=ident )
+        return DeferredAttr( self, ident )
 
     def __getitem__( self, item ):
-        return type(self)( self, _item=item )
+        return DeferredSubscript( self, item )
 
     def __call__( self, *args, **kwArgs ):
-        return type(self)( self, _args=(args, kwArgs) )
+        return DeferredCall( self, (args, kwArgs) )
 
     def __str__( self ):
-        def _recurse( obj ):
-            if isinstance( obj, Deferred ):
-                return _recurse( obj._obj ) + str(vars(obj))
-            return getattr( obj, '__name__', None ) or str(obj)
-        return _recurse( self )
+        return getattr( self._obj, '__name__', None ) or str(self._obj)
 
     def __value__( self ):
-        obj = self.__resolve__( self._obj )
-        if self._attr:
-            return getattr( obj, self.__resolve__( self._attr ) )
-        if self._args:
-            return obj( *self.__resolve__( self._args[0] ), **self.__resolve__( self._args[1] ) )
-        if self._eval:
-            return eval( obj, *self._eval )
-        if self._item:
-            return obj[ self.__resolve__( self._item ) ]
-        return obj
+        return self.__resolve__( self._obj )
 
     @classmethod
     def __resolve__( _class, obj ):
         if isinstance( obj, Deferred ):
-            return obj.value
+            return obj.__value__()
         try:
             len(obj)
             try:
@@ -135,3 +118,48 @@ class Deferred( ContextEvaluable, metaclass=DeferredType ):
         except TypeError:
             pass
         return obj
+
+
+#----------------------------------
+class DeferredAttr( Deferred ):
+#----------------------------------
+    def __value__( self ):
+        obj = self.__resolve__( self._obj )
+        return getattr( obj, self.__resolve__( self._data ) )
+
+    def __str__( self ):
+        return '{0._obj}.{0._data}'.format( self )
+
+
+#----------------------------------
+class DeferredCall( Deferred ):
+#----------------------------------
+    def __value__( self ):
+        obj = self.__resolve__( self._obj )
+        return obj( *self.__resolve__( self._data[0] ), **self.__resolve__( self._data[1] ) )
+
+    def __str__( self ):
+        args = ', '.join( (*map( str, self._data[0] ), *(f'{k}={v}' for k,v in self._data[1].items())) )
+        return '{0._obj}( {1} )'.format( self, args )
+
+
+#--------------------------------------
+class DeferredSubscript( Deferred ):
+#--------------------------------------
+    def __value__( self ):
+        obj = self.__resolve__( self._obj )
+        return obj[self.__resolve__( self._data )]
+
+    def __str__( self ):
+        return '{0._obj}[ {0._data} ]'.format( self )
+
+
+#---------------------------------
+class DeferredEval( Deferred ):
+#---------------------------------
+    def __value__( self ):
+        obj = self.__resolve__( self._obj )
+        return eval( obj, *self._data )
+
+    def __str__( self ):
+        return 'eval({0._obj})'.format( self )
