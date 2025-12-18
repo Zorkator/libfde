3e0eec249f3b6a21f8536076fbb65f7bb3f9faf6
Josef Scheuer <josef.scheuer@grs.de>
2025-12-18 12:39:06 +0100

    update test_simulator

--- test/test_simulator.F90
+++ test/test_simulator.F90
@@ -1,6 +1,9 @@
 
 #include "fde/itfUtil.fpp"
-#include "fde/scope.fpp"
+#include "fde/string.fpp"
+
+# define __rootLocator__   _this_file_basename()
+# include "fde/scope.fpp"
 
 !--------------------
 module sim_reflection
@@ -13,31 +16,21 @@ module sim_reflection
   use fde_streamvisitor
   implicit none
 
+  type(StreamVisitor_t)    :: fout
   type(HashMap_t), pointer :: root_scope => null()
   type(HashMap_t), pointer :: hook_scope => null()
-# undef _callHook
-# define _callHook(id) \
-    call invokeCallback( hook_scope, id )
-# undef _ALLOCATE
-# define _ALLOCATE( scope, sym, dim )  \
-    allocate( sym dim )               ;\
-    _set_scopeSymbol( scope, sym )
+  integer                  :: istat
 
-  contains
+contains
 
   subroutine init_reflection()
-    type(StreamVisitor_t) :: streamer
-
     if (.not. associated(root_scope)) then
-      root_scope => getScope('test_simulator')
-      hook_scope => getScope( root_scope, 'hooks' )
-
+      root_scope => __rootScope__
+      hook_scope => __hookScope__
       call declareCallback( hook_scope, 'start' )
       call declareCallback( hook_scope, 'step' )
       call declareCallback( hook_scope, 'finish' )
-
-      streamer = StreamVisitor(0)
-      call accept( getScope(), streamer%super )
+      fout = StreamVisitor(0)
     endif
   end subroutine
 end module
@@ -73,12 +66,12 @@ module sim_data
     dt  = 0.1
     te  = 10.0
     cnt = 0
-    _ALLOCATE( scope, real4_array,  (10) );    real4_array  = 0;
-    _ALLOCATE( scope, real4_matrix, (10,20) ); real4_matrix = 1;
-    _ALLOCATE( scope, id_array,     (10) );    id_array     = ' '
-    _ALLOCATE( scope, id_table,     (6,7) );   id_table     = ' '
-    _ALLOCATE( scope, name_array,   (20) );    name_array   = ' ' !<< FIXME: id_array and name_array share the same typeinfo, whose byteSize
-                                                                  !           is set to the size of the first ref'ed table ...
+    _REALLOCATE_visible( scope, real4_array,  (5) );   real4_array  = 0;
+    _REALLOCATE_visible( scope, real4_matrix, (5,3) ); real4_matrix = 1;
+    _REALLOCATE_visible( scope, id_array,     (10) );  id_array     = ' '
+    _REALLOCATE_visible( scope, id_table,     (3,5) ); id_table     = ' '
+    _REALLOCATE_visible( scope, name_array,   (10) );  name_array   = ' ' !<< FIXME: id_array and name_array share the same typeinfo, whose byteSize
+                                                                          !           is set to the size of the first ref'ed table ...
     do i = 1,size(id_array)
       write( id_array(i), '(i10)' ) i
     end do
@@ -86,6 +79,9 @@ module sim_data
     do i = 1,size(name_array)
       write( name_array(i), '(A4,I5)' ) 'name', i
     end do
+
+    ! print process scope ...
+    call accept( getScope(), fout%super )
   end subroutine
 end module
 
@@ -104,10 +100,11 @@ subroutine run_c()
   ! main loop
   do while (t <= te)
     _callHook('step')
-    write(6,*,iostat=ios) "t: ", t
-    cnt = cnt + 1
-    real4_array(mod(cnt, size(real4_array)) + 1) = cnt
-    t = t + dt
+      ! perform timestep ...
+      write(6,*,iostat=ios) "t: ", t
+      cnt = cnt + 1
+      real4_array(mod(cnt, size(real4_array)) + 1) = cnt
+      t = t + dt
   end do
 
   _callHook('finish')
